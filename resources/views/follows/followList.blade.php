fbc45eff204f2e1205f07d4036ac6a8fac79a70f
LAPTOP-UEPAPHB5\shotaro shiwa <1110shotaro@gmail.com>
2022-06-24 06:38:26 +0900

    テスト文章をいれました

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.login')
-
 @section('content')
-
+<h2>followList</h2>
 @endsection
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.login')
-
 @section('content')
+<h2>followerList</h2>
 @endsection
